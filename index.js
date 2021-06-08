1b7074648d0a75dc3dc3539e6640b9a1f4d12c56
UmimChoi <umimchoi@gmail.com>
2021-06-08 12:31:01 +0700

    server 100% done

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+var bodyParser = require('body-parser')
+const { request } = require('express')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+var bankrefs = []
+var sms = []
+app.get('/bankref', (req, res) => {
+  res.send(bankrefs)
+})
+app.post('/bankref', (req, res) => {
+  var bankref = request.body
+  if(!bankref || bankref.text == ""){
+    res.status(500).send({error: "Your bankref must have text"})
+  } else {
+    bankrefs.push(bankref)
+    res.status(200).send(bankref)
+  }
+})
+app.get('/sms', (req, res) => {
+  res.send(sms)
+})
+app.post('/sms', (req, res) => {
+  var data = request.body
+  if(!data || data.text == ""){
+    res.status(500).send({error: "Your sms must not be blank"})
+  } else {
+    bankrefs.push(data)
+    res.status(200).send(data)
+  }
+})
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
